c774d4da8765d9598b7f7b206ba93b354b0f6ef5
Boukari Yameogo <yameogo.boukari@gmail.com>
2024-12-22 01:18:55 -0500

    adding internal ingress

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,30 +1,35 @@
 locals {
-  do_token = var.do_token
-  environment = var.environment
-  do_spaces_access_id = var.do_spaces_access_id
-  do_spaces_secret_key = var.do_spaces_secret_key
-  public_key = var.public_key
-  private_key = var.private_key
-  project = var.project
-  region = var.region
-  subnet = var.subnet
-  tools_path = abspath("tools")
+	do_token = var.do_token
+	environment = var.environment
+	do_spaces_access_id = var.do_spaces_access_id
+	do_spaces_secret_key = var.do_spaces_secret_key
+	public_key = var.public_key
+	private_key = var.private_key
+	project = var.project
+	region = var.region
+	subnet = var.subnet
+	tools_path = abspath("tools")
 
-  prefix = "${local.project}-${local.region}-${local.environment}"
+	prefix = "${local.project}-${local.region}-${local.environment}"
 
-  ingress_controller_namespace = "nginx"
-  certificate_manager_namespace = "cert-manager"
-  namespaces = toset([
-    local.ingress_controller_namespace,
-    local.certificate_manager_namespace
-  ])
+	ingress_controller_namespace = "nginx"
+	certificate_manager_namespace = "cert-manager"
+	app_namespace = "backend"
+	monitoring_namespace = "monitoring"
 
-  k8s_version = "1.31.1-do.5"
-  loadbalancer_name = "nginx-load-balancer"
+	namespaces = toset([
+		local.ingress_controller_namespace,
+		local.certificate_manager_namespace,
+		local.app_namespace,
+		local.monitoring_namespace
+	])
 
-  ttl = 1800
-  txt_verification_value = var.txt_verification_value
-  mx_value = var.mx_value
+	k8s_version = "1.31.1-do.5"
+	loadbalancer_name = "nginx-load-balancer"
 
-  domain = "wouritech.com"
+	ttl = 1800
+	txt_verification_value = var.txt_verification_value
+	mx_value = var.mx_value
+
+	domain = "wouritech.com"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -40,11 +40,6 @@ provider "digitalocean" {
   spaces_secret_key = local.do_spaces_secret_key
 }
 
-#provider "kubernetes" {
-#  host =digitalocean_kubernetes_cluster.this.endpoint
-#  token = digitalocean_kubernetes_cluster.this.kube_config[0].token
-#  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
-#}
 
 provider "helm" {
     kubernetes {
