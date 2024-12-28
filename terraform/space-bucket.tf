23a6cf10b144b0bd2cd74ee27d913d3d1501aee0
Boukari Yameogo <yameogo.boukari@gmail.com>
2024-12-28 11:04:33 -0500

    full working

--- terraform/space-bucket.tf
+++ terraform/space-bucket.tf
@@ -7,10 +7,19 @@ resource "digitalocean_certificate" "this" {
   name = "${local.project}-${local.environment}-cdn-certificate"
   type = "lets_encrypt"
   domains = ["${local.project}-${local.environment}-static.${local.domain}"]
+
+  depends_on = [
+    digitalocean_domain.this,
+  ]
 }
 
 resource "digitalocean_cdn" "this" {
   origin = digitalocean_spaces_bucket.this.bucket_domain_name
   custom_domain = "${local.project}-${local.environment}-static.${local.domain}"
   certificate_name = digitalocean_certificate.this.name
+
+    depends_on = [
+        digitalocean_certificate.this,
+        digitalocean_spaces_bucket.this,
+    ]
 }
