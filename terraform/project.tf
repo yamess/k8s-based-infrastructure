52dfbc82d5b67962b264c04487dbd1bf70121e43
Boukari Yameogo <yameogo.boukari@gmail.com>
2024-12-29 15:32:38 -0500

    fix

--- terraform/project.tf
+++ terraform/project.tf
@@ -5,6 +5,7 @@ resource "digitalocean_project" "this" {
     digitalocean_kubernetes_cluster.this.urn,
     digitalocean_domain.this.urn,
     digitalocean_spaces_bucket.this.urn,
-    digitalocean_database_cluster.redis.urn
+    digitalocean_database_cluster.redis.urn,
+    digitalocean_database_cluster.postgres-cluster.urn
   ]
 }
